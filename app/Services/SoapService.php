f0d0f33495b5c8b9487e55cef7f6c7962a9378b2
nachad0ng <nachadong@gmail.com>
2026-08-06 15:19:39 +0700

    add Service class

--- app/Services/SoapService.php
+++ app/Services/SoapService.php
@@ -1,3 +1,7 @@
+<?php
+
+namespace App\Services;
+
 class SoapService
 {
     protected SoapClient $client;
---
+++ config/soap.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'wsdl' => env('SOAP_WSDL'),
+    'username' => env('SOAP_USERNAME'),
+    'password' => env('SOAP_PASSWORD'),
+];
