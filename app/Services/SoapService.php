87b052a8dfeb7085b3473491368dcc19cb29afd6
nachad0ng <nachadong@gmail.com>
2026-08-06 11:56:36 +0700

    first commit

---
+++ app/Services/SoapService.php
@@ -0,0 +1,26 @@
+class SoapService
+{
+    protected SoapClient $client;
+
+    public function __construct()
+    {
+        $this->client = new SoapClient(
+            config('services.soap.wsdl')
+        );
+    }
+
+    public function execute($database,$sql)
+    {
+        $command = "
+
+            use [$database];
+
+            $sql
+
+        ";
+
+        return $this->client->ExecuteSQL([
+            'Command'=>$command
+        ]);
+    }
+}
---
+++ database/migrations/2026_08_06_041654_create_sync_jobs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sync_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('database');
+            $table->longText('sql');
+            $table->integer('interval');
+            $table->timestamp('last_execute')->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sync_jobs');
+    }
+};
